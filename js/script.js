8ecf594a5862d13b98ffaf180a5ca15a8024b298
galaykostudio <65346908+galaykostudio@users.noreply.github.com>
2020-05-14 15:35:11 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$('.sliderclass').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: true,
+  dots: true,
+  infinite: true,
+  speed: 500,
+  autoplay: false,
+  autoplaySpeed: 3000,
+  centerMode: true,
+  variableWidth: true
+});
+
+
+
+
